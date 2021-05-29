9fd8824a1e6ed7dc56be10fad3d8f8b275fa7b81
Juan Jose Castellanos <juancho.312@hotmail.com>
2021-05-28 20:44:59 -0500

    add other test case

--- test/test.foo.cpp
+++ test/test.foo.cpp
@@ -6,3 +6,9 @@ TEST_CASE("test the execution of foo", "[foo]")
   fmt::print("Test the execution of foo\n");
   REQUIRE( 1 == 1);
 }
+
+TEST_CASE("test the execution of bar", "[bar]")
+{
+  fmt::print("Test the execution of bar\n");
+  REQUIRE( 1 == 1);
+}
