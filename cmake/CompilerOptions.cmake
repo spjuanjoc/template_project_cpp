8e8a5342f2c1e90d3806c16006ce2512fc74bcff
Juan Jose Castellanos <juancho.312@hotmail.com>
2021-06-24 17:22:48 -0500

    build: reorder options alphabetically

--- cmake/CompilerOptions.cmake
+++ cmake/CompilerOptions.cmake
@@ -13,7 +13,6 @@ function(set_project_options project_options_target_name)
       /w14263       # 'function': member function does not override any base class virtual member function
       /w14265       # 'classname': class has virtual functions, but destructor is not virtual instances of this class may not be destructed correctly
       /w14287       # 'operator': unsigned/negative constant mismatch
-      /we4289       # nonstandard extension used: 'variable': loop control variable declared in the for-loop is used outside the for-loop scope
       /w14296       # 'operator': expression is always 'boolean_value'
       /w14311       # 'variable': pointer truncation from 'type1' to 'type2'
       /w14545       # expression before comma evaluates to a function which is missing an argument list
@@ -27,31 +26,32 @@ function(set_project_options project_options_target_name)
       /w14905       # wide string literal cast to 'LPSTR'
       /w14906       # string literal cast to 'LPWSTR'
       /w14928       # illegal copy-initialization; more than one user-defined conversion has been implicitly applied
+      /we4289       # nonstandard extension used: 'variable': loop control variable declared in the for-loop is used outside the for-loop scope
       )
 
   set(CLANG_OPTIONS
       -Wall
       -pedantic             # warn if non-standard C++ is used
-      -Wextra               # reasonable and standard
-      -Wshadow              # warn the user if a variable declaration shadows one from a parent context
-      -Wnon-virtual-dtor    # warn the user if a class with virtual functions has a non-virtual destructor. This helps catch hard to track down memory errors
-      -Wold-style-cast      # warn for c-style casts
       -Wcast-align          # warn for potential performance problem casts
-      -Wunused              # warn on anything being unused
-      -Woverloaded-virtual  # warn if you overload (not override) a virtual function
       -Wconversion          # warn on type conversions that may lose data
-      -Wsign-conversion     # warn on signed-unsigned conversions
-      -Wnull-dereference    # warn if a null dereference is detected
       -Wdouble-promotion    # warn if float is implicit promoted to double
+      -Wextra               # reasonable and standard
       -Wformat=2            # warn on security issues around functions that format output (ie printf)
+      -Wnon-virtual-dtor    # warn the user if a class with virtual functions has a non-virtual destructor. This helps catch hard to track down memory errors
+      -Wnull-dereference    # warn if a null dereference is detected
+      -Wold-style-cast      # warn for c-style casts
+      -Woverloaded-virtual  # warn if you overload (not override) a virtual function
+      -Wshadow              # warn the user if a variable declaration shadows one from a parent context
+      -Wsign-conversion     # warn on signed-unsigned conversions
+      -Wunused              # warn on anything being unused
       )
 
   set(GCC_OPTIONS
       ${CLANG_OPTIONS}
-      -Wmisleading-indentation  # warn if indentation implies blocks where blocks do not exist
-      -Wduplicated-cond         # warn if if / else chain has duplicated conditions
       -Wduplicated-branches     # warn if if / else branches have duplicated code
+      -Wduplicated-cond         # warn if if / else chain has duplicated conditions
       -Wlogical-op              # warn about logical operations being used where bitwise were probably wanted
+      -Wmisleading-indentation  # warn if indentation implies blocks where blocks do not exist
       -Wuseless-cast            # warn if you perform a cast to the same type
       )
 
