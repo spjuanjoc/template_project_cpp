81d400e812ccf766f22d91995dc5899bfe845a94
Juan Jose Castellanos <juancho.312@hotmail.com>
2021-06-24 17:26:13 -0500

    feat: add Foo call

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,23 @@
-#include "fmt/core.h"
-//#include <iostream>
+/**
+ * @brief   Entry point.
+ *
+ * @author  juan.castellanos
+ * @date    2021-06-24
+ */
+
+#include "Greet/Foo.h"
+#include <fmt/core.h>
+#include <memory>
+
+using namespace Greet;
 
 int main()
 {
-//  std::cout << "Hello, World!\n";
   fmt::print("Hello, World!\n");
+  auto foo = std::make_unique<Foo>(42);
+
+  foo->greet();
+  foo->greetFromPlatform();
+
   return 0;
 }
