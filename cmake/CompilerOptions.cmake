c9f3de2df7cfa3e6f83bef4d68f9e45898bd3c4d
spjuanjoc <juancho.312@hotmail.com>
2022-09-30 18:30:41 -0500

    build: update the compiler options/warnings

--- cmake/CompilerOptions.cmake
+++ cmake/CompilerOptions.cmake
@@ -1,95 +1,140 @@
-# from: https://github.com/lefticus/cppbestpractices/blob/master/02-Use_the_Tools_Available.md
+#
+# File: CompilerOptions.cmake
+#
+# Date:   2021-05-31
+# Author: spjuanjoc
+#
+# Based upon: https://github.com/lefticus/cppbestpractices/blob/master/02-Use_the_Tools_Available.md
+#
+# Function that sets the compiler options for the given target
+# This function must be called after add_library()/add_executable() instructions.
+#
+# See the warnings documentation:
+#   MSVC  https://learn.microsoft.com/en-us/cpp/error-messages/compiler-warnings/compiler-warnings-by-compiler-version?view=msvc-170
+#   clang https://clang.llvm.org/docs/UsersManual.html
+#   gcc   https://gcc.gnu.org/onlinedocs/gcc/Warning-Options.html
+#
+# Usage:
+#   set_compiler_options(${TARGET_NAME})
 #
 
-function(set_project_options project_options_target_name)
+function(set_compiler_options TARGET_NAME)
 
   set(MSVC_OPTIONS
-      /W4           # Baseline reasonable warnings
-      /permissive-  # standards conformance mode for MSVC compiler.
-      /w14242       # 'identifier': conversion from 'type1' to 'type1', possible loss of data
-      /w14254       # 'operator': conversion from 'type1:field_bits' to 'type2:field_bits', possible loss of data
-      /w14263       # 'function': member function does not override any base class virtual member function
-      /w14265       # 'classname': class has virtual functions, but destructor is not virtual instances of this class may not be destructed correctly
-      /w14287       # 'operator': unsigned/negative constant mismatch
-      /w14296       # 'operator': expression is always 'boolean_value'
-      /w14311       # 'variable': pointer truncation from 'type1' to 'type2'
-      /w14545       # expression before comma evaluates to a function which is missing an argument list
-      /w14546       # function call before comma missing argument list
-      /w14547       # 'operator': operator before comma has no effect; expected operator with side-effect
-      /w14549       # 'operator': operator before comma has no effect; did you intend 'operator'?
-      /w14555       # expression has no effect; expected expression with side- effect
-      /w14619       # pragma warning: there is no warning number 'number'
-      /w14640       # Enable warning on thread un-safe static member initialization
-      /w14826       # Conversion from 'type1' to 'type_2' is sign-extended. This may cause unexpected runtime behavior.
-      /w14905       # wide string literal cast to 'LPSTR'
-      /w14906       # string literal cast to 'LPWSTR'
-      /w14928       # illegal copy-initialization; more than one user-defined conversion has been implicitly applied
-      /we4289       # nonstandard extension used: 'variable': loop control variable declared in the for-loop is used outside the for-loop scope
-      )
+    /W4
+    /permissive-
+    /w14242
+    /w14254
+    /w14263
+    /w14265
+    /w14287
+    /we4289
+    /w14296
+    /w14311
+    /w14545
+    /w14546
+    /w14547
+    /w14549
+    /w14555
+    /w14619
+    /w14640
+    /w14826
+    /w14905
+    /w14906
+    /w14928
+  )
+
+  set(COMMON_OPTIONS
+    -Wall
+    -Wextra
+    -pedantic
+
+    -Wcast-align
+    -Wconversion
+    -Wdelete-incomplete
+    -Wdelete-non-virtual-dtor
+    -Wdouble-promotion
+    -Wnon-virtual-dtor
+    -Wnull-dereference
+    -Wold-style-cast
+    -Woverloaded-virtual
+    -Wunused
+    -Wshadow
+    -Wsign-conversion
+    -Wvarargs
+  )
 
   set(CLANG_OPTIONS
-      -Wall
-      -pedantic             # warn if non-standard C++ is used
-      -Wcast-align          # warn for potential performance problem casts
-      -Wconversion          # warn on type conversions that may lose data
-      -Wdouble-promotion    # warn if float is implicit promoted to double
-      -Wextra               # reasonable and standard
-      -Wformat=2            # warn on security issues around functions that format output (ie printf)
-      -Wnon-virtual-dtor    # warn the user if a class with virtual functions has a non-virtual destructor. This helps catch hard to track down memory errors
-      -Wnull-dereference    # warn if a null dereference is detected
-      -Wold-style-cast      # warn for c-style casts
-      -Woverloaded-virtual  # warn if you overload (not override) a virtual function
-      -Wshadow              # warn the user if a variable declaration shadows one from a parent context
-      -Wsign-conversion     # warn on signed-unsigned conversions
-      -Wunused              # warn on anything being unused
-      )
+    ${COMMON_OPTIONS}
+    -Weverything
+    -Wdangling-initializer-list
+#    -Wdocumentation
+    -Wdynamic-class-memaccess
+    -Winvalid-noreturn
+    -Wunsequenced
+    -Wuser-defined-literals
+    -Wvexing-parse
+
+    -Wno-class-varargs
+    -Wno-c99-extensions
+    -Wno-c++98-compat   # in everything
+    -Wno-documentation  # in everything
+    -Wno-undefined-var-template
+  )
 
   set(GCC_OPTIONS
-      ${CLANG_OPTIONS}
-      -Wduplicated-branches     # warn if if / else branches have duplicated code
-      -Wduplicated-cond         # warn if if / else chain has duplicated conditions
-      -Wlogical-op              # warn about logical operations being used where bitwise were probably wanted
-      -Wmisleading-indentation  # warn if indentation implies blocks where blocks do not exist
-      -Wuseless-cast            # warn if you perform a cast to the same type
-      )
+    ${COMMON_OPTIONS}
+    -pedantic-errors
+    -Wcast-qual
+    -Wctor-dtor-privacy
+    -Wdeprecated
+    -Wdeprecated-declarations
+    -Wduplicated-branches
+    -Wduplicated-cond
+    -Wfloat-equal
+    -Winvalid-offsetof
+    -Wlogical-op
+    -Wpacked
+    -Wpointer-arith
+    -Wredundant-decls
+    -Wundef
+#    -Wunused-macros # Not enabled due to an {fmt} issue https://github.com/fmtlib/fmt/pull/2651
+    -Wuseless-cast
 
-  if (ENABLE_WARN_AS_ERR)
+    -Wno-class-memaccess
+    -Wno-long-long
+  )
+
+
+  ## Warnings as errors
+  if (ENABLE_WERROR)
+    set(MSVC_OPTIONS  ${MSVC_OPTIONS}   /WX)
     set(CLANG_OPTIONS ${CLANG_OPTIONS}  -Werror)
     set(GCC_OPTIONS   ${GCC_OPTIONS}    -Werror)
-    set(MSVC_OPTIONS  ${MSVC_OPTIONS}   /WX)
   endif ()
 
-  if (MSVC)
-    set(PROJECT_OPTIONS ${MSVC_OPTIONS})
-    set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS True PARENT_SCOPE) # Allow DLL symbols generation
-    set(BUILD_SHARED_LIBS True PARENT_SCOPE)
-    message(STATUS "Windows export all symbols: ${CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS}")
-    message(STATUS "Windows Build shared libs: ${BUILD_SHARED_LIBS}")
-    target_compile_definitions(${project_options_target_name}
-        PUBLIC
-          CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=True
-          BUILD_SHARED_LIBS=True
-        )
-  elseif (CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
-    set(PROJECT_OPTIONS ${CLANG_OPTIONS})
-    set(CMAKE_MACOSX_RPATH False PARENT_SCOPE) # Disable rpath for dylib
-    message(STATUS "OSX disable rpath: ${CMAKE_MACOSX_RPATH}")
-  elseif (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-    set(PROJECT_OPTIONS ${GCC_OPTIONS})
-    target_link_libraries(${project_options_target_name} PRIVATE pthread)
-    if (ENABLE_COVERAGE)
+
+  ## Set the options for each compiler.
+  if(MSVC)
+    set(COMPILER_OPTIONS ${MSVC_OPTIONS})
+  elseif(CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
+    set(COMPILER_OPTIONS ${CLANG_OPTIONS})
+  elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    set(COMPILER_OPTIONS ${GCC_OPTIONS})
+    if(ENABLE_COVERAGE)
       set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --coverage" PARENT_SCOPE) # PARENT_SCOPE required since this is called from a function
       message(STATUS "coverage enabled: ${CMAKE_CXX_FLAGS}")
-    endif ()
-  else ()
+    endif()
+  else()
     message(AUTHOR_WARNING "No compiler warnings set for '${CMAKE_CXX_COMPILER_ID}' compiler.")
-  endif ()
+  endif()
 
-  target_compile_options(${project_options_target_name} PRIVATE ${PROJECT_OPTIONS}) # for header-only use INTERFACE
+  target_compile_options(${TARGET_NAME} PRIVATE $<$<COMPILE_LANGUAGE:CXX>:${COMPILER_OPTIONS}>) # for header-only use INTERFACE
 
-  message(STATUS "Project options: ${PROJECT_OPTIONS}")
-  message(STATUS "Compiler: ${CMAKE_CXX_COMPILER}")
-  message(STATUS "Compiler ID: ${CMAKE_CXX_COMPILER_ID}")
-  message(STATUS "Platform: ${CMAKE_SYSTEM_NAME}")
+  message (STATUS "Target name:     ${TARGET_NAME}")
+  message (STATUS "Target options:  ${COMPILER_OPTIONS}")
+  message (STATUS "Compiler:        ${CMAKE_CXX_COMPILER}")
+  message (STATUS "Compiler ID:     ${CMAKE_CXX_COMPILER_ID}")
+  message (STATUS "Platform:        ${CMAKE_SYSTEM_NAME}")
 
 endfunction()
