7fe1733e632dc9d4f7e88ad308a944029d3a4008
spjuanjoc <juancho.312@hotmail.com>
2025-09-20 12:22:52 -0500

    test(win): disable extra options

--- test/Foo/test.Foo.cxx
+++ test/Foo/test.Foo.cxx
@@ -18,11 +18,11 @@ using namespace Greet;
 
 TEST_CASE("test the execution of foo in Windows", "[foo]")
 {
-  /*const auto foo = std::make_unique<Foo>(42);
+  const auto foo = std::make_unique<Foo>(42);
 
   foo->greetFromPlatform();
   const std::string_view platform = foo->getPlatform();
-
+/*
   fmt::print("Foo in {}\n", platform);
 
   REQUIRE(platform == "Windows");*/
